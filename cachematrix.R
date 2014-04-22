9e5455f86422b1ef147b2af6752608321186b11c
Mariann <managy.hu@gmail.com>
2014-04-22 12:42:24 -0400

    Peer Assignment II
    I worked on the peer assignment and believe that my functions are
    functioning correctly now.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## With the following two functions - 'makeCacheMatrix' and 
+## 'cacheSolve' - first you can create a matrix object, then
+## calculate its inverse. If the matrix's inverse was already
+## calculated - and the matrix has not changed - the function
+## (to save time) will retrive the inverse from the cache and  
+## won't run the computation. 
 
-## Write a short comment describing this function
+## This function - makeCacheMatrix - creates a special "matrix"
+## object that can cash its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y = matrix()) {
+    x <<- y 
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function - casheSolve - computes the inverse of the special
+## "matrix" returned by makeCacheMatrix above. If the inverse has
+## already been calculated (and the matrix has not changed), then
+## the cacheSolve function will retrive the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
